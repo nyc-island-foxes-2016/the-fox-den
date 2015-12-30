330732cd0196cf52ba02a14d100b7fcda6f6a516
Sam Blackman <sblackman@gmail.com>
2015-12-30 17:11:59 -0500

    add commented example of foreign key aliases

--- db/migrate/20151229131422_create_foxes.rb
+++ db/migrate/20151229131422_create_foxes.rb
@@ -4,7 +4,9 @@ class CreateFoxes < ActiveRecord::Migration
       t.string     "name", null: false
       t.string     "color", default: "red"
       t.integer    "den_id", index: true
-
+      # t.references    "den", index: true
+      # t.belongs_to    "den", index: true
+      # # the two lines above do exactly the same thing as line 6
       t.timestamps null: false
     end
   end
