9313481c92ce39bc35781978a69d8547b43bd87b
Sam Blackman <sblackman@gmail.com>
2015-12-30 17:36:08 -0500

    add char limit constraints to fields in foxes and dens

--- db/migrate/20151229131422_create_foxes.rb
+++ db/migrate/20151229131422_create_foxes.rb
@@ -1,8 +1,8 @@
 class CreateFoxes < ActiveRecord::Migration
   def change
     create_table   "foxes" do |t|
-      t.string     "name", null: false
-      t.string     "color", default: "red"
+      t.string     "name", null: false, limit: 50
+      t.string     "color", default: "red", limit: 20
       t.integer    "den_id", index: true
       # t.references    "den", index: true
       # t.belongs_to    "den", index: true
