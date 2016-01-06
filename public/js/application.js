67194762c143bd5c3c59000e796ed6bfd1e1dc10
Sam Blackman <sblackman@gmail.com>
2016-01-06 10:55:06 -0500

    add color morphing and event delegation

--- public/js/application.js
+++ public/js/application.js
@@ -2,15 +2,30 @@ $(document).on("ready", function() {
   $("#toggle-form-button").on("click", function() {
     $("#main-page-fox-form-container").toggle();
   });
+
   $("#main-page-fox-form-container form").on("submit", function(event) {
     event.preventDefault();
-    debugger;
     $.ajax({
       type: "POST",
       url: "/foxes",
       data: $(this).serialize()
     }).done(function(response) {
-      $("body").append(response);
+      $("#fox-container").append(response);
     });
   });
+
+  $("#fox-container").on("click", ".color-change",function(event) {
+    event.preventDefault();
+    $(this).parent().prev().prev().css("color", getRandomColor());
+  })
+
 });
+
+function getRandomColor() {
+  var letters = '0123456789ABCDEF'.split('');
+  var color = '#';
+  for (var i = 0; i < 6; i++ ) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
