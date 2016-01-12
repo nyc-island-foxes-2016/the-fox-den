92e3bbe884911a7bcace7d0e08bf345a54782aa0
Zack Mance <zmance@gmail.com>
2016-01-12 16:06:58 -0500

    Adds custom validator code

--- app/models/fox.rb
+++ app/models/fox.rb
@@ -3,8 +3,11 @@ class Fox < ActiveRecord::Base
 
   belongs_to :den
 
+  validate :start_with_fox
+  
   validates :name, length: {maximum: 50}, presence: true, uniqueness: true
   validates :color, length: {maximum: 20}
+
 
   def password
     @password ||= Password.new(password_hash) # reading encrypted password and turning it into BCrypt Password object
@@ -13,5 +16,13 @@ class Fox < ActiveRecord::Base
   def password=(new_password) # plain text password incoming here
     @password = Password.create(new_password) # plain text password encrypted here
     self.password_hash = @password # password_hash column/attribute set to encrypted password here
+  end
+
+  private
+
+  def start_with_fox
+    unless self.name.starts_with?('fox')
+      errors.add(:name, "HEY, Your name should start with fox")
+    end
   end
 end
